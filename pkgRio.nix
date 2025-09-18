e3ff3f7409bdcc1b6f8f3e999c428ca85e398e2d
Kai Giokas <kai.giokas@creyos.com>
2025-09-18 15:53:08 -0500

    Update nix package
    Include separate terminfo output, flags based on withX11 or
    withWayland

--- pkgRio.nix
+++ pkgRio.nix
@@ -22,21 +22,22 @@
   withX11 ? !stdenv.isDarwin,
   withWayland ? !stdenv.isDarwin,
   ...
-}: let
+}:
+let
   cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
   rustPlatform = makeRustPlatform {
     cargo = rust-toolchain;
     rustc = rust-toolchain;
   };
   rlinkLibs =
-    if stdenv.isDarwin
-    then [
-      darwin.libobjc
-      darwin.apple_sdk_11_0.frameworks.AppKit
-      darwin.apple_sdk_11_0.frameworks.AVFoundation
-      darwin.apple_sdk_11_0.frameworks.MetalKit
-      darwin.apple_sdk_11_0.frameworks.Vision
-    ]
+    if stdenv.isDarwin then
+      [
+        darwin.libobjc
+        darwin.apple_sdk_11_0.frameworks.AppKit
+        darwin.apple_sdk_11_0.frameworks.AVFoundation
+        darwin.apple_sdk_11_0.frameworks.MetalKit
+        darwin.apple_sdk_11_0.frameworks.Vision
+      ]
     else
       [
         (lib.getLib gcc-unwrapped)
@@ -56,51 +57,58 @@
         wayland
       ];
 in
-  rustPlatform.buildRustPackage {
-    inherit (cargoToml.workspace.package) version;
-    name = "rio";
-    src = ./.;
-    cargoLock.lockFile = ./Cargo.lock;
+rustPlatform.buildRustPackage {
+  inherit (cargoToml.workspace.package) version;
+  name = "rio";
+  src = ./.;
+  cargoLock.lockFile = ./Cargo.lock;
 
-    cargoBuildFlags = "-p rioterm";
+  cargoBuildFlags = "-p rioterm";
 
-    buildInputs = rlinkLibs;
-    runtimeDependencies = rlinkLibs;
+  buildInputs = rlinkLibs;
+  runtimeDependencies = rlinkLibs;
 
-    nativeBuildInputs =
-      [
-        ncurses
-      ]
-      ++ lib.optionals stdenv.isLinux [
-        pkg-config
-        cmake
-        autoPatchelfHook
-      ];
-    
-    postInstall = ''
-      install -D -m 644 misc/rio.desktop -t $out/share/applications
-      install -D -m 644 misc/logo.svg \
-                        $out/share/icons/hicolor/scalable/apps/rio.svg
-  
-      # Install terminfo files
-      install -dm 755 "$out/share/terminfo/r/"
-      tic -xe xterm-rio,rio,rio-direct -o "$out/share/terminfo" misc/rio.terminfo
-    '' + lib.optionalString stdenv.hostPlatform.isDarwin ''
-      mkdir $out/Applications/
-      mv misc/osx/Rio.app/ $out/Applications/
-      mkdir $out/Applications/Rio.app/Contents/MacOS/
-      ln -s $out/bin/rio $out/Applications/Rio.app/Contents/MacOS/
-    '';
+  nativeBuildInputs = [
+    ncurses
+  ]
+  ++ lib.optionals stdenv.isLinux [
+    pkg-config
+    cmake
+    autoPatchelfHook
+  ];
 
-    
-    buildNoDefaultFeatures = true;
-    buildFeatures = ["x11" "wayland"];
-    meta = {
-      description = "A hardware-accelerated GPU terminal emulator focusing to run in desktops and browsers";
-      homepage = "https://rioterm.com";
-      license = lib.licenses.mit;
-      platforms = lib.platforms.unix;
-      changelog = "https://github.com/raphamorim/rio/blob/master/CHANGELOG.md";
-      mainProgram = "rio";
-    };
-  }
+  outputs = [
+    "out"
+    "terminfo"
+  ];
+
+  postInstall = ''
+    install -D -m 644 misc/rio.desktop -t $out/share/applications
+    install -D -m 644 misc/logo.svg \
+                      $out/share/icons/hicolor/scalable/apps/rio.svg
+
+    # Install terminfo files
+    install -dm 755 "$terminfo/share/terminfo/r/"
+    tic -xe xterm-rio,rio,rio-direct -o "$terminfo/share/terminfo" misc/rio.terminfo
+  ''
+  +
+    lib.optionalString stdenv.hostPlatform.isDarwin
+      # sh
+      ''
+        mkdir $out/Applications/
+        mv misc/osx/Rio.app/ $out/Applications/
+        mkdir $out/Applications/Rio.app/Contents/MacOS/
+        ln -s $out/bin/rio $out/Applications/Rio.app/Contents/MacOS/
+      '';
+
+  buildNoDefaultFeatures = true;
+  buildFeatures = (lib.optionals withX11 [ "x11" ]) ++ (lib.optionals withWayland [ "wayland" ]);
+  meta = {
+    description = "A hardware-accelerated GPU terminal emulator focusing to run in desktops and browsers";
+    homepage = "https://rioterm.com";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.unix;
+    changelog = "https://github.com/raphamorim/rio/blob/master/CHANGELOG.md";
+    mainProgram = "rio";
+  };
+}
